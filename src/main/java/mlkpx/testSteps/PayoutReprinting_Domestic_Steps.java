20fcf8aa3799d6cf59a332359b76c659af98166b
Albert Monceda <albert.monceda@mlhuillier.com>
2024-06-27 08:43:39 +0800

    retry change into 3 times

--- src/main/java/mlkpx/testSteps/PayoutReprinting_Domestic_Steps.java
+++ src/main/java/mlkpx/testSteps/PayoutReprinting_Domestic_Steps.java
@@ -23,7 +23,7 @@ public class PayoutReprinting_Domestic_Steps extends Base_Steps{
         waitSleep(2000);
         reader.refreshTestData();
         waitSleep(10000);
-        String payoutKPTN = reader.getSuccessPayoutKPTN();
+        String payoutKPTN = reader.getPayoutKPTN();
         type(payoutCancellationPageObjects.RepKTPN(), "KTPN Number",payoutKPTN);
         type(payoutCancellationPageObjects.ReasonReprinting(), "Reason for Reprinting ", "Testing");
         click(payoutCancellationPageObjects.Reprint(), "Search Button");
